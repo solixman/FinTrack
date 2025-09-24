feed208be9ff7f1d5449f7ba7f779489210f03c9
soulayman <soulaymanjaa09@gmail.com>
2025-09-24 10:09:07 +0100

    created models

---
+++ migrations/20250924083956-create-transaction.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.FLOAT
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      note: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  }
+};
---
+++ models/budget.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Budget extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Budget.init({
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+    limitAmount: DataTypes.FLOAT,
+    month: DataTypes.STRING,
+    date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Budget',
+  });
+  return Budget;
+};
