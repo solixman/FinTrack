2aec08b77e1f936059cd3d3326a182e08b56ea3b
soulayman <soulaymanjaa09@gmail.com>
2025-10-01 22:28:52 +0100

    profile page rendering method created

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+const {User}= require('../models');
+
+
+module.exports= {
+
+
+    async Profile(req,res){
+
+        try {
+            let user = await User.findByPk(req.session.user.id); 
+            
+       const profileHTML = await require('ejs').renderFile(__dirname + '/../views/pages/profile.ejs', {
+                transactions,
+                categories
+            });
+            
+            return res.render('../views/index.ejs', {
+                error: req.flash('error'),
+                message: req.flash('message'),
+                title: 'Profile',
+                user,
+                body: profileHTML
+            });
+
+            
+        } catch (error) {
+             req.flash('error', "something went wrong");
+            return res.redirect(req.get('referer') || '/dashboard');
+        }
+        }
+        
+
+}
